733ece4e9c128c7a69b7f9637187bba2d5dc2943
GoodPointt <119069023+GoodPointt@users.noreply.github.com>
2023-05-05 13:16:49 +0000

    Deploying to gh-pages from @ GoodPointt/goit-js-hw-09@8e81f2e24e01eff8d212ef4b4d27156278cb3d21 🚀

---
+++ 01-color-switcher.dbc8b68c.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let n=null;function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e(),n=setInterval((()=>{e()}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.stopBtn.disabled=!0,t.startBtn.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.dbc8b68c.js.map
