8e81f2e24e01eff8d212ef4b4d27156278cb3d21
Dmytro Petyshyn <dev6012@gmail.com>
2023-05-05 16:16:10 +0300

    1st task complited

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+const INTERVAL = 1000;
+let timerId = null;
+
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  changeBgColor();
+
+  timerId = setInterval(() => {
+    changeBgColor();
+  }, INTERVAL);
+}
+
+function onStopClick() {
+  refs.stopBtn.disabled = true;
+  refs.startBtn.disabled = false;
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
